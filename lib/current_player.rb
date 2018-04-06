6c600e1ff0d2a77e74aa08d43fef80ced4ee22db
Michael Blandford <mdblandford@gmail.com>
2018-04-06 13:18:31 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,13 +5,13 @@ def turn_count(board)
         counter += 1 
       end
     end
-    return counter
+    counter
   end 
   
   def current_player(board)
-    if turn_count(board) % 2 == 0
+    if turn_count(board) .even?
       "X"
-    else turn_count(board) % 2 == 1 
+    else turn_count(board) .odd? 
       "O"
     end
   end
