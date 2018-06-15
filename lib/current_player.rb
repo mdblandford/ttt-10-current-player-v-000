c2771b96f42e661ba92312639b1f3a747645314b
Michael Blandford <mdblandford@gmail.com>
2018-06-15 17:10:41 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,13 @@
 def turn_count(board)
-  counter = 0 
+  counter = 0
     board.each do |player|
       if player == "X" || player == "O"
-        counter += 1 
+        counter += 1
       end
     end
     counter
-  end 
-  
+  end
+
   def current_player(board)
-    if turn_count(board) .even?
-      "X"
-    else turn_count(board) .odd? 
-      "O"
-    end
+    turn_count(board).even? ? "X" : "O"
   end
