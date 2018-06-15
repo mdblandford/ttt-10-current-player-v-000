a80133879dd4be6888a148390c8455dfd02dd41b
Michael Blandford <mdblandford@gmail.com>
2018-06-15 17:06:59 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,5 +9,5 @@ def turn_count(board)
   end
 
   def current_player(board)
-    turn_count(board) .even? ? "X" : turn_count(board) .odd? ? "O"
+    turn_count(board).even? ? "X" : turn_count(board).odd? ? "O"
   end
