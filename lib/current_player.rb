7985dc5bdd261d9e91cf81e78866b3fe471576d2
Michael Blandford <mdblandford@gmail.com>
2018-06-15 17:05:43 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,13 @@
 def turn_count(board)
-  counter = 0 
-    board.each do |player| 
+  counter = 0
+    board.each do |player|
       if player == "X" || player == "O"
-        counter += 1 
+        counter += 1
       end
     end
     counter
-  end 
-  
+  end
+
   def current_player(board)
     turn_count(board) .even? ? "X" : turn_count(board) .odd? ? "O"
   end
